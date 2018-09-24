f7aa40ec2c705d9611488ac23717779cb574bcab
jsphbtst <jsphbtst@protonmail.ch>
2018-09-25 00:11:53 +0800

    bootstrap basic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component }  from 'react';
+import { isMobile } from 'react-device-detect';
 import './App.css';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			width: window.innerWidth * 0.60,
-			height: window.innerHeight * 0.60
+			selected: false
 		}
+		this.renderContent = this.renderContent.bind(this);
+		this.renderFurnitureDetails = this.renderFurnitureDetails.bind(this);
 	}
 
-	render() {
+	renderFurnitureDetails() {
+		// Add a switch statement here
+	}
+	
+	renderContent() {
+		if(isMobile) {
+			return(
+				<div className='Mobile'>
+					<h1>Please use the browser.</h1>
+				</div>
+			);
+		}
 		return(
-			<div className='App'>
-				<h2 className='title'>EgyE 211 Engineering Library 1 Pre-Energy Audit</h2>
+			<div>
 				<div className='row'>
 					<div className='col-sm-1'></div>
+					
 					<div className='col-sm-10'>
 						<div className='embed-responsive embed-responsive-16by9'>
 							<iframe className='embed-responsive-item'
@@ -27,8 +40,59 @@ class App extends Component {
 								title='autodesk'/>
 						</div>
 					</div>
+
 					<div className='col-sm-1'></div>
 				</div>
+
+				<div className='row'>
+					<div className='col-sm-1'></div>
+					
+					<div className='col-sm-4'>
+						<div className='pull-left'>
+							<h2>Select Equipment</h2>
+						</div>
+						{ /* 
+
+						Clickables Here 
+						- Other icons lose opacity when one is hovered on
+						- onClick={() => this.renderFurnitureDetails(furnitureName)}
+						- pick a default furniture that's clicked
+	
+						*/ }
+					</div>
+
+					<div className='col-sm-2'></div>
+
+					<div className='col-sm-4'>
+						<div className='pull-left'>
+							<h2>Equipment Details</h2>
+						</div>
+						{ this.renderFurnitureDetails() }
+					</div>
+
+					<div className='col-sm-1'></div>
+				</div>
+
+				<div className='row'>
+					<div className='col-sm-1'></div>
+					
+					<div className='col-sm-10'>
+						<div className='pull-left'>
+							<h2>Recommendations</h2>
+						</div>
+						{ /* Insert recommendations here */ }
+					</div>
+
+					<div className='col-sm-1'></div>
+				</div>
+			</div>
+		);
+	}
+	render() {
+		return(
+			<div className='App'>
+				<h2 className='title'>EgyE 211 Engineering Library 1 Pre-Energy Audit</h2>
+				{ this.renderContent() }
 			</div>
 		);
 	}
